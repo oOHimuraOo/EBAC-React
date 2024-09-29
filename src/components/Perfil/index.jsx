652b083f990f6380f1f68ec0351004be7063d7f1
Felipe Cerqueira <felipecer06@gmail.com>
2024-09-29 17:46:22 -0300

    Resolvendo problema

--- src/components/Perfil/index.jsx
+++ src/components/Perfil/index.jsx
@@ -6,7 +6,7 @@ const Perfil = ({nome, avatar, usuario}) => {
             <div className={styles.container}>
                 <img className={styles.perfil_avatar} src={avatar} />
                 <h1 className={styles.perfil_titulo} >{usuario}</h1>
-                <h2 className={styles.perfil_tiulo, styles.h2}>{nome}</h2>
+                <h2 className={styles.perfil_tiulo}>{nome}</h2>
             </div>
         </header>
     )
